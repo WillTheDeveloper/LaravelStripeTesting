9f71620ce89ec04c595ad5ed7d0d90b9d7517511
Will <will@teamatlas.cloud>
2021-08-21 15:02:34 +0100

    Installed tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,7 @@
-welcome!
+<!doctype html>
+<head>
+    <!-- ... --->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
